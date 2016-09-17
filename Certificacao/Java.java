f671368ec4b6b33db461d481990cb661cddbb741
João  Correia <joaoadspibax@gmail.com>
2016-09-17 10:23:02 -0300

    no message

---
+++ Certificacao/Java.java
@@ -0,0 +1,12 @@
+
+class Java{
+
+	public static void main(String... args){
+
+		for(String st: args){
+			System.out.print(st);
+		}
+	}
+
+
+}
---
+++ Certificacao/Joao.java
@@ -0,0 +1,15 @@
+
+public class Joao{
+	
+	public static void main(String[] args){
+
+
+		for(String st:args){
+			System.out.print(st);
+		}
+
+		Teste teste = new Teste();
+
+	}
+
+}
